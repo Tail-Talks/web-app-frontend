77307315afa37f954760ccb7c09c3d0c5330e10b
Alexander Glebov <le.xander.gl@gmail.com>
2024-12-02 18:04:49 +0100

    [#86bzrx539] Добавлен компонент Личного профиля

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
+import React from 'react';
 
-import './App.css'
+import './App.css';
 // import Footer from './Components/Footer/Footer'
 // import Header from './Components/Header/Header'
 // import LoginUserForm from './Components/LoginUserForm/LoginUserForm'
 // import ProfileDescriptionForm from './Components/ProfileDescriptionForm/ProfileDescriptionForm'
 // import UserName from './Components/UserName/UserName'
-import LoginPage from './Pages/LoginPage/LoginPage'
+import LoginPage from './Pages/LoginPage/LoginPage';
+// import PersonalAccount from './Pages/PersonalAccount/PersonalAccount';
 function App() {
-
   return (
     <>
-     {/* <Header/> */}
-    <LoginPage/>
-    {/* <Footer/> */}
-    {/* <UserName/>
+      {/* <Header/> */}
+      <LoginPage />
+      {/* <Footer/> */}
+      {/* <UserName/>
     <ProfileDescriptionForm/>
     <LoginUserForm/> */}
+      {/* <PersonalAccount /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
